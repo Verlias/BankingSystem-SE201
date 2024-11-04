b3a4ab4ebcb980eadda9098a310e979a629df02d
Logan <loganvoravong@gmail.com>
2024-11-04 00:51:00 -0500

    Fixed CD Class Inheritance

--- src/main/java/CertificateOfDeposit.java
+++ src/main/java/CertificateOfDeposit.java
@@ -1,8 +1,8 @@
 public class CertificateOfDeposit extends Accounts {
 
     public CertificateOfDeposit(double balance, double apr, String id) {
-        super(balance, 0, id);
-        setApr(apr);
+        super(balance, apr, id);
+
     }
 
 }
